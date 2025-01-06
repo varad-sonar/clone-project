eb5c8350887573de514bf28af2726f30268d60e5
varad-sonar <varadsonar2@gmail.com>
2025-01-06 13:44:11 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 12345
+let accountEmail = "om@gmail.com"
+var accountPassword = "121212"
+accountCity = "mumbai"
+let accountstate;
+
+// accountId ="54321" //
+
+accountEmail ="var@gmail.com"
+accountPassword = "212121"
+accountCity = "pune"
+
+/*
+prefer not to use var
+becouse of issue in block scope and functional scope 
+*/
+
+
+// console.log(accountId);
+
+console.table( [accountId, accountEmail, accountPassword, accountCity, accountstate])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3) //  we are using nodejs, not  browser
+
+console.log(3 + 3); 
+
+console.log("varad")
+
+let name = "varad"
+let age = 18
+let isLoggedIn = true/false // any one //
+
+/* datatypes*/
+/* primitive datatypes*/
+// string => '',"" frefer to use ""
+// number => 2 to di power 52
+// bigint
+// boolean => true/false
+// null => standalone value 
+// undefined 
+// symbol => uniqueness
+
+
+/*non-primitive datatypes*/
+// object =>
+
+console.log( typeof undefined); // undefined 
+console.log( typeof null); // object
+
+
+
+
